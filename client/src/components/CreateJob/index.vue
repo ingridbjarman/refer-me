a10eff76d87dfe5fa663cb15ff0cbb9e0846f014
ingrid <bjarmaningrid@gmail.com>
2020-11-23 11:24:11 +0000

    CreateJob component started

---
+++ client/src/components/CreateJob/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <v-sheet
+          rounded="lg"
+          class="mb-3 p-3 mx-auto">
+          hello ingrid
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+    name: 'JobPage',
+}
+</script>
